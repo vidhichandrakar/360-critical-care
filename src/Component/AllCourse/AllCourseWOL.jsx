c248d8f372f7d9d1569b5062af7efb74bf7bac5c
Bittu singh <bittusingh8991@gmail.com>
2024-07-07 02:12:31 +0530

    AllCourseCard

--- src/Component/AllCourse/AllCourseWOL.jsx
+++ src/Component/AllCourse/AllCourseWOL.jsx
@@ -1,30 +1,34 @@
 import { Box, Button, Typography } from "@mui/material";
 import react, { Fragment } from "react";
-import homeimg from "../../Media/ProjectImage/AllCourseHome.jpg"
-import cardimg from "../../Media/ProjectImage/AllCourseCard.jpg"
-import "../../CSS/Courses.css"
+import homeimg from "../../Media/ProjectImage/AllCourseHome.jpg";
+import cardimg from "../../Media/ProjectImage/AllCourseCard.jpg";
+import NewsLetter from "../../Media/ProjectImage/AllCourseImg3.jpg";
+import JoinImg from "../../Media/ProjectImage/AllCourseImg4.jpg";
+import "../../CSS/Courses.css";
+import {AllCourseWOLCard , PopularCard} from "../../JsonData/JsonData"
 
 const AllCourseWOL = () => {
     return (
         <Fragment>
             <Box className="HeadimgBox">
-                <img src={homeimg} />
+                <img src={homeimg} width="100%"/>
             </Box>
             <Box className="CoursesCardMainBox">
-                <Box className="CradBox">
+                {AllCourseWOLCard?.length && AllCourseWOLCard?.map((item) => {
+                  return   <Box className="CradBox">
                     <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
+                    <Box className="CardTextBox"    >
                         <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
+                           {item.heading}
                         </Typography>
                         <Box className="Price_ButtonBox">
                             <Box >
                                 <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
+                                   {item.Special}
                                 </Typography>
                                 <Box className="PriceBox">
                                 <Typography sx={{fontWeight: 600}}>
-                                    $17999
+                                    {item.price}
                                 </Typography>
                                 <Typography sx={{ml:"100px", color: "grey",}}>
                                     $30000
@@ -41,277 +45,36 @@ const AllCourseWOL = () => {
                         </Box>
                     </Box>
                 </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox">
-                        <Typography className="CardHeading">
-                            TRICS-1: PreParatory course for EDIC-1 2nd session 2024
-                        </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                    Special Discounted Price
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    $17999
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFF
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
-                    </Box>
-                </Box>
+                })}
             </Box>
+            <Box>
+                <img src={JoinImg} width="100%"/>
+            </Box>
+            <Box className="PopularCourseMainBox">
+                <Typography>Our <span>Popular Course</span></Typography>
+               
+                <Box className="PopularCourseBox">
+                     {PopularCard.map((item) => {
+                    return  <Box className="PopularCardBox">
+                        <img src={cardimg} width={380} height={150}/>
+                        <Box>
+                            <Typography>
+                           {item.Para1}
+                            </Typography>
+                            <Typography>
+                            {item.Para2}
+                            </Typography>
+                        </Box>
+                    </Box>
+
+           
+                })}
+                    </Box>
+                    
+            </Box>
+            <Box>
+                        <img src={NewsLetter} width="100%"/>
+                    </Box>
         </Fragment>
     )
 }
