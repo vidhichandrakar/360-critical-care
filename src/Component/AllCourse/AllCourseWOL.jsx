f894300f64b906d7b7f858865d1ba354a8ec650d
Shrishti2698 <shrishti260498@gmail.com>
2024-07-09 07:24:58 +0530

    YouTube section added in homepagesss

--- src/Component/AllCourse/AllCourseWOL.jsx
+++ src/Component/AllCourse/AllCourseWOL.jsx
@@ -5,78 +5,73 @@ import cardimg from "../../Media/ProjectImage/AllCourseCard.jpg";
 import NewsLetter from "../../Media/ProjectImage/AllCourseImg3.jpg";
 import JoinImg from "../../Media/ProjectImage/AllCourseImg4.jpg";
 import "../../CSS/Courses.css";
-import {AllCourseWOLCard , PopularCard} from "../../JsonData/JsonData"
+import { AllCourseWOLCard, PopularCard } from "../../JsonData/JsonData";
 
 const AllCourseWOL = () => {
-    return (
-        <Fragment>
-            <Box className="HeadimgBox">
-                <img src={homeimg} width="100%"/>
-            </Box>
-            <Box className="CoursesCardMainBox">
-                {AllCourseWOLCard?.length && AllCourseWOLCard?.map((item) => {
-                  return   <Box className="CradBox">
-                    <img src={cardimg} width={300} height={150}/>
-                    <Box className="CardTextBox"    >
-                        <Typography className="CardHeading">
-                           {item.heading}
+  return (
+    <Fragment>
+      <Box className="HeadimgBox">
+        <img src={homeimg} width="100%" />
+      </Box>
+      <Box className="CoursesCardMainBox">
+        {AllCourseWOLCard?.length &&
+          AllCourseWOLCard?.map((item) => {
+            return (
+              <Box className="CradBox">
+                <img src={cardimg} width={300} height={150} />
+                <Box className="CardTextBox">
+                  <Typography className="CardHeading">
+                    {item.heading}
+                  </Typography>
+                  <Box className="Price_ButtonBox">
+                    <Box>
+                      <Typography sx={{ fontWeight: 600 }}>
+                        {item.Special}
+                      </Typography>
+                      <Box className="PriceBox">
+                        <Typography sx={{ fontWeight: 600 }}>
+                          {item.price}
                         </Typography>
-                        <Box className="Price_ButtonBox">
-                            <Box >
-                                <Typography sx={{fontWeight: 600}}>
-                                   {item.Special}
-                                </Typography>
-                                <Box className="PriceBox">
-                                <Typography sx={{fontWeight: 600}}>
-                                    {item.price}
-                                </Typography>
-                                <Typography sx={{ml:"100px", color: "grey",}}>
-                                    $30000
-                                </Typography>
-                                <Typography sx={{ml:6}}>
-                                    40% OFFs
-                                </Typography>
-
-                                </Box>
-                            </Box>
-                            <Button variant="contained">
-                                Explore Now
-                            </Button>
-                        </Box>
+                        <Typography sx={{ ml: "100px", color: "grey" }}>
+                          $30000
+                        </Typography>
+                        <Typography sx={{ ml: 6 }}>40% OFFs</Typography>
+                      </Box>
                     </Box>
+                    <Button variant="contained">Explore Now</Button>
+                  </Box>
                 </Box>
-                })}
-            </Box>
-            <Box>
-                <img src={JoinImg} width="100%"/>
-            </Box>
-            <Box className="PopularCourseMainBox">
-                <Typography>Our <span>Popular Course</span></Typography>
-               
-                <Box className="PopularCourseBox">
-                     {PopularCard.map((item) => {
-                    return  <Box className="PopularCardBox">
-                        <img src={cardimg} width={380} height={150}/>
-                        <Box>
-                            <Typography>
-                           {item.Para1}
-                            </Typography>
-                            <Typography>
-                            {item.Para2}
-                            </Typography>
-                        </Box>
-                    </Box>
+              </Box>
+            );
+          })}
+      </Box>
+      <Box>
+        <img src={JoinImg} width="100%" />
+      </Box>
+      <Box className="PopularCourseMainBox">
+        <Typography>
+          Our <span>Popular Course</span>
+        </Typography>
 
-           
-                })}
-                    </Box>
-                    
-            </Box>
-            <Box>
-                        <img src={NewsLetter} width="100%"/>
-                    </Box>
-        </Fragment>
-    )
-}
+        <Box className="PopularCourseBox">
+          {PopularCard.map((item) => {
+            return (
+              <Box className="PopularCardBox">
+                <img src={cardimg} width={380} height={150} />
+                <Box>
+                  <Typography>{item.Para1}</Typography>
+                  <Typography>{item.Para2}</Typography>
+                </Box>
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
+      <Box>
+        <img src={NewsLetter} width="100%" />
+      </Box>
+    </Fragment>
+  );
+};
 
 export default AllCourseWOL;
