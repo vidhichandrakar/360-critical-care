27f77965c720718d7d11e91b1f6005a746b7ac81
Shrishti2698 <shrishti260498@gmail.com>
2024-07-17 03:59:46 +0530

    contactus

--- src/Component/Service/ContactUs.js
+++ src/Component/Service/ContactUs.js
@@ -2,16 +2,19 @@ import React, { Fragment } from "react";
 import "./ContactUs.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { FAQData } from "../../JsonData/AboutData/AboutData";
 
 import Divider from "@mui/material/Divider";
+import contacUsFirstImg from "../../Media/Images/contactUsFirstImg.png";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
 function ContactUs() {
   return (
     <Fragment>
       <Header />
-      <Box className="ContactBox">
+      {/* <Box className="ContactBox">
         <Box className="ContactTaxt ContactFont">
           <h4 className="Heading Headcenter">
             Contact <span className="AquaColor">Us</span>
@@ -253,6 +256,174 @@ function ContactUs() {
               <p className="divider"></p>
             </Box>
           ))}
+        </Box>
+      </Box> */}
+      <Box className="ContactBox">
+        <img
+          src={contacUsFirstImg}
+          className="contacUsFirstImg"
+        />
+
+        <Box className="getInTouchCompleteBox">
+          <Box className="getInTouch">
+            <Typography className="getInTouch-head">
+              <b>
+                Get in <span className="AquaColor">Touch</span>
+              </b>
+            </Typography>
+            <Typography sx={{ mt: 2, mb: 5, fontSize: "0.9rem" }}>
+              Our team is composed of dedicated professionals with a passion for
+              education. Each member brings a unique set of skills and expertise
+              to ensure the highest quality of learning for our students.
+            </Typography>
+
+            {/* <Box className="getInTouchPersonalInfo"> */}
+            <Box className="getInTouch-about">
+              <Box className="getInTouchAbtSection">
+                <Box className="getInTouchVerticalLine"></Box>
+                <Typography sx={{ fontSize: "0.9rem" }}>
+                  Plot/Flat No-502, Adhikary Arcade, Bariatu Road, Bariantu
+                  Ranchi, Jharkhand
+                </Typography>
+              </Box>
+
+              <Box className="getInTouchAbtSection">
+                <Box className="getInTouchVerticalLine"></Box>
+                <Box>
+                  <Typography sx={{ fontSize: "0.9rem" }}>
+                    +91-7070993343
+                  </Typography>
+                  <Typography sx={{ fontSize: "0.9rem" }}>
+                    +91-9348938039
+                  </Typography>
+                </Box>
+              </Box>
+
+              <Box className="getInTouchAbtSection">
+                <Box className="getInTouchVerticalLine"></Box>
+                <Typography sx={{ fontSize: "0.9rem" }}>
+                  www.360criticalcare.com
+                </Typography>
+              </Box>
+
+              <Box className="getInTouchAbtSection">
+                <Box className="getInTouchVerticalLine"></Box>
+                <Typography sx={{ fontSize: "0.9rem" }}>
+                  360criticalcare@gmail.com
+                </Typography>
+              </Box>
+            </Box>
+            {/* </Box> */}
+          </Box>
+
+          <Box className="getInTouch-right">
+            <Box className="getInTouch-right-nameAndMob">
+              <Box>
+                <Typography>
+                  <b>NAME</b>
+                </Typography>
+                <Stack
+                  component="form"
+                  sx={{
+                    width: "58ch",
+                    height: "8ch",
+                  }}
+                  spacing={2}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    hiddenLabel
+                    id="filled-hidden-label-small"
+                    size="small"
+                  />
+                </Stack>
+              </Box>
+
+              <Box sx={{ ml: 3 }}>
+                <Typography>
+                  <b>MOBILE</b>
+                </Typography>
+                <Stack
+                  component="form"
+                  sx={{
+                    width: "61ch",
+                  }}
+                  spacing={2}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    hiddenLabel
+                    id="filled-hidden-label-small"
+                    size="small"
+                  />
+                </Stack>
+              </Box>
+            </Box>
+
+            <Box>
+              <Typography>
+                <b>EMAIL</b>
+              </Typography>
+              <Stack
+                component="form"
+                sx={{
+                  width: "122ch",
+                  height: "8ch",
+                }}
+                spacing={2}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  size="small"
+                />
+              </Stack>
+            </Box>
+
+            <Box>
+              <Typography>
+                <b>SEND MESSAGE</b>
+              </Typography>
+              <Stack
+                component="form"
+                sx={{
+                  width: "122ch",
+                  // padding: "20px"
+                }}
+                spacing={2}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField hiddenLabel id="filled-hidden-label-normal" />
+              </Stack>
+            </Box>
+
+            {/* <Box  sx={{ flexGrow: 0 ,position:"fixed", right:0}} */}
+            {/* className="contactUs-sendMsgBtn"> */}
+            <Button variant="contained" className="sendMsgButton">
+              Send message
+            </Button>
+            {/* </Box> */}
+
+            {/* <Box><Button>Send message</Button></Box> */}
+          </Box>
+        </Box>
+
+        {/* MAP  */}
+        <Box>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29293.035269702268!2d85.33562851618916!3d23.40162025020351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e15783b9c0b5%3A0x5c22411ea37e8259!2sBariatu%2C%20Ranchi%2C%20Jharkhand%20834009!5e0!3m2!1sen!2sin!4v1710535703912!5m2!1sen!2sin"
+            //  width="1600"
+            //  height="450"
+            className="contactUsMap"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </Box>
       </Box>
       <Footer />
