6735fe5ae7a3a51a8361bf258617074486c69cc6
Rajdeepak24 <rajdeepakvishwakarma24@gmail.com>
2024-10-24 01:05:46 +0530

    added contact us api call with validation

--- src/Component/ApiFactory/ApiAction.jsx
+++ src/Component/ApiFactory/ApiAction.jsx
@@ -1,43 +1,54 @@
-import axios from "axios"
+import axios from "axios";
 import { APIS } from "./ApiConstant";
 
 export const getCategory = ({ callBack }) => {
-    const url = APIS.getCategory;
-    axios.get(url).then((response) => {
-      callBack(response);
-    });
-  };    
+  const url = APIS.getCategory;
+  axios.get(url).then((response) => {
+    callBack(response);
+  });
+};
 export const getBlog = ({ callBack }) => {
-    const url = APIS.getBlog;
-    axios.get(url).then((response) => {
+  const url = APIS.getBlog;
+  axios.get(url).then((response) => {
+    callBack(response);
+  });
+};
+
+export const getAllCourses = ({ callBack, searchString, error }) => {
+  let url = new URL(`${APIS.allCourses}`);
+  if (searchString) {
+    url.searchParams.set("course_name", searchString);
+  }
+  axios
+    .get(url)
+    .then((response) => {
       callBack(response);
+    })
+    .catch((errorMessage) => {
+      error(errorMessage);
     });
-  };    
+};
 
-export const getAllCourses = ({callBack, searchString, error }) => {
-    let url = new URL(`${APIS.allCourses}`);
-    if(searchString){
-      url.searchParams.set("course_name", searchString);
-    }  
-    axios
-      .get(url)
-      .then((response) => {
-  
-        callBack(response);
-      })
-      .catch((errorMessage) => {
-        error(errorMessage);
-      });
-  };
+export const addNewsLetter = ({ payload, callBack, error }) => {
+  const url = APIS.addNewsLetter;
+  axios
+    .post(url, payload)
+    .then((response) => {
+      callBack(response);
+    })
+    .catch((errorMessage) => {
+      error(errorMessage);
+    });
+};
 
-  export const addNewsLetter = ({payload, callBack, error }) => {
-    const url = APIS.addNewsLetter;
-    axios
-      .post(url, payload)
-      .then((response) => {
-        callBack(response);
-      })
-      .catch((errorMessage) => {
-        error(errorMessage);
-      });
-  };
+export const contactUsDetails = ({ payload, callBack, error }) => {
+  const url = APIS.contactUS;
+  axios
+    .post(url, payload)
+    .then((response) => {
+      callBack(response);
+    })
+    .catch((errorMessage) => {
+      error(errorMessage);
+    });
+};
--- src/Component/Service/COntactus.jsx
+++ src/Component/Service/COntactus.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./ContactUs.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -10,24 +10,82 @@ import contacUsFirstImg from "../../Media/Images/contactUsFirstImg.png";
 import MobileViewHeadimg from "../../Media/Images/AllcourseHeadImg.png";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
-
+import { contactUsDetails } from "../ApiFactory/ApiAction";
 
 const COntactus = () => {
-    return (
-        <Fragment>
+  const [formValues, setFormValues] = useState({
+    name: "",
+    mobile: "",
+    email: "",
+    message: "",
+  });
+  const [formErrors, setFormErrors] = useState({
+    mobile: "",
+    email: "",
+  });
+  const handleInputChange = (e, type) => {
+    console.log("e->", e, "type--------->", type);
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const validateMobile = (mobile) => {
+    const mobilePattern = /^\d{10}$/;
+    return mobilePattern.test(mobile);
+  };
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    const mobileError = validateMobile(formValues.mobile)
+      ? ""
+      : "Mobile number must be exactly 10 digits.";
+    const emailError = validateEmail(formValues.email)
+      ? ""
+      : 'Email must contain "@" and "." in the correct format.';
+
+    setFormErrors({
+      mobile: mobileError,
+      email: emailError,
+    });
+
+    if (!mobileError && !emailError) {
+      console.log("Validation passed:", formValues);
+      let payload = {
+        Name: formValues.name,
+        email: formValues.email,
+        mobile_no: formValues.mobile,
+        message: formValues.message,
+      };
+      contactUsDetails({
+        payload: payload,
+        callBack: (response) => {
+          console.log("----->", response);
+        },
+      });
+      // Add logic to handle successful validation
+    }
+  };
+  return (
+    <Fragment>
       <Header />
       {/* <img
-          src={contacUsFirstImg}
-          className="contacUsFirstImg"
-        /> */}
-        <div className="homeImage">
-          <img src={contacUsFirstImg} className="WebViewImg"/>
-          <img src={MobileViewHeadimg} className="MobileViewImg" />
-        </div>
+            src={contacUsFirstImg}
+            className="contacUsFirstImg"
+          /> */}
+      <div className="homeImage">
+        <img src={contacUsFirstImg} className="WebViewImg" />
+        <img src={MobileViewHeadimg} className="MobileViewImg" />
+      </div>
       <Box className="ContactMainBox">
-      
         <Box className="FormBox">
-            <Box className="getInTouch">
+          <Box className="getInTouch">
             <Typography className="getInTouch-head">
               <b>
                 Get in <span className="AquaColor">Touch</span>
@@ -74,65 +132,123 @@ const COntactus = () => {
                 </Typography>
               </Box>
             </Box>
-            </Box>
+          </Box>
 
-            <Box className="DetailsFormBox">
-                <Box sx={{display: "flex", flexDirection: "row", textAlign: "left"}}>
-                   <Box sx={{display: "flex", flexDirection: "column"}}>
-                   <Typography>Name</Typography>
-                   <TextField
-                    hiddenLabel
-                    id="filled-hidden-label-small"
-                    size="small"
-                    className="textfieldName"
-                  />
-                   </Box>
-                   <Box sx={{ml: 2}}>
-                   <Typography>Mobile</Typography>
-                   <TextField
-                    hiddenLabel
-                    id="filled-hidden-label-small"
-                    size="small"
-                    className="textfieldName"
-                  />
-                   </Box>
-                </Box>
-                <Box>
-                <Box sx={{display: "flex", flexDirection: "column", textAlign: "left", mt: 2}}>
-                   <Typography>Email</Typography>
-                   <TextField
-                    hiddenLabel
-                    id="filled-hidden-label-small"
-                    size="small"
-                    sx={{backgroundColor: "rgba(232, 232, 232, 0.549)"}}
-                  />
-                </Box>
-                <Box sx={{display: "flex", flexDirection: "column", textAlign: "left", mt: 2}}>
-                   <Typography>Send Message</Typography>
-                   <TextField
-                    id="outlined-multiline-flexible"
-                    multiline
-                    sx={{backgroundColor: "rgba(232, 232, 232, 0.549)"}}
-                    rows={5}
-                  />
-                </Box>
-                </Box>
-                <Button variant="contained" className="sendMsgButton">
+          <Box className="DetailsFormBox">
+            {/* <form onSubmit={handleSubmit}> */}
+            <Box
+              sx={{ display: "flex", flexDirection: "row", textAlign: "left" }}
+            >
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Typography>Name</Typography>
+                <TextField
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  size="small"
+                  className="textfieldName"
+                  name="name"
+                  onChange={handleInputChange}
+                  value={formValues.name}
+                />
+              </Box>
+              <Box sx={{ ml: 2 }}>
+                <Typography>Mobile</Typography>
+                <TextField
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  size="small"
+                  className="textfieldName"
+                  name="mobile"
+                  value={formValues.mobile}
+                  onChange={handleInputChange}
+                  error={!!formErrors.mobile}
+                  helperText={formErrors.mobile}
+                  type="number"
+                  onInput={(e) => {
+                    // Prevents more than 10 digits from being entered
+                    if (e.target.value.length > 10) {
+                      e.target.value = e.target.value.slice(0, 10);
+                    }
+                  }}
+                  sx={{
+                    "& input[type=number]": {
+                      "-moz-appearance": "textfield", // Firefox
+                    },
+                    "& input[type=number]::-webkit-outer-spin-button": {
+                      "-webkit-appearance": "none",
+                      margin: 0,
+                    },
+                    "& input[type=number]::-webkit-inner-spin-button": {
+                      "-webkit-appearance": "none",
+                      margin: 0,
+                    },
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  textAlign: "left",
+                  mt: 2,
+                }}
+              >
+                <Typography>Email</Typography>
+                <TextField
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  size="small"
+                  sx={{ backgroundColor: "rgba(232, 232, 232, 0.549)" }}
+                  name="email"
+                  value={formValues.email}
+                  onChange={handleInputChange}
+                  error={!!formErrors.email}
+                  helperText={formErrors.email}
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  textAlign: "left",
+                  mt: 2,
+                }}
+              >
+                <Typography>Send Message</Typography>
+                <TextField
+                  id="outlined-multiline-flexible"
+                  multiline
+                  sx={{ backgroundColor: "rgba(232, 232, 232, 0.549)" }}
+                  rows={5}
+                  name="message"
+                  value={formValues.message}
+                  onChange={handleInputChange}
+                />
+              </Box>
+            </Box>
+            <Button
+              variant="contained"
+              onClick={handleSubmit}
+              className="sendMsgButton"
+            >
               Send message
             </Button>
-            </Box>
+            {/* </form> */}
+          </Box>
         </Box>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29293.035269702268!2d85.33562851618916!3d23.40162025020351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e15783b9c0b5%3A0x5c22411ea37e8259!2sBariatu%2C%20Ranchi%2C%20Jharkhand%20834009!5e0!3m2!1sen!2sin!4v1710535703912!5m2!1sen!2sin"
-            className="contactUsMap"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </Box>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29293.035269702268!2d85.33562851618916!3d23.40162025020351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e15783b9c0b5%3A0x5c22411ea37e8259!2sBariatu%2C%20Ranchi%2C%20Jharkhand%20834009!5e0!3m2!1sen!2sin!4v1710535703912!5m2!1sen!2sin"
+          className="contactUsMap"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </Box>
       <Footer />
-      </Fragment>
-    )
-}
+    </Fragment>
+  );
+};
 
-export default COntactus
+export default COntactus;
